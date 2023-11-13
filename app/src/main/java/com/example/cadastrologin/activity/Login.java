a45931e82fef64558eef1e661e130115fa937cc5
millyvasc <camillejaninecte@gmail.com>
2023-11-13 18:23:19 -0300

    Login with Google

--- app/src/main/java/com/example/cadastrologin/activity/Login.java
+++ app/src/main/java/com/example/cadastrologin/activity/Login.java
@@ -17,8 +17,19 @@ import android.widget.Toast;
 import com.example.cadastrologin.R;
 import com.example.cadastrologin.dao.UserDAO;
 import com.example.cadastrologin.model.User;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class Login extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    Button btn_google;
+
     Button btnLogin;
     EditText txtmail, txtpass;
     TextView tvSign;
@@ -26,6 +37,21 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_google = findViewById(R.id.btn_google);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
+        // -----------------------------------
 
         SharedPreferences sp = getSharedPreferences("MoonUsers", Context.MODE_PRIVATE);
         String auxMail = sp.getString("mail", "");
@@ -109,5 +135,33 @@ public class Login extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    void navigateToSecondActivity(){
+        finish();
+        Intent intent = new Intent(Login.this, Main.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/cadastrologin/activity/Main.java
+++ app/src/main/java/com/example/cadastrologin/activity/Main.java
@@ -1,5 +1,6 @@
 package com.example.cadastrologin.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -12,63 +13,94 @@ import android.widget.TextView;
 import com.example.cadastrologin.R;
 import com.example.cadastrologin.dao.UserDAO;
 import com.example.cadastrologin.model.User;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Main extends AppCompatActivity {
+
     TextView mail, username;
-    Button btnAlter, btnDelete, btnLogout;
+    Button btnAlter, btnDelete, btnLogout, btn_google;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
 
     @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
         SharedPreferences sp = getSharedPreferences("MoonUsers", Context.MODE_PRIVATE);
-        String auxMail = sp.getString("mail", "");
+//        String auxMail = sp.getString("mail", "");
+//
+//        User user = new User();
+//        user.setMail(auxMail);
+//        UserDAO uDAO = new UserDAO(getApplicationContext(), user);
+//        user = uDAO.getUserByMail();
+//
+        mail = findViewById(R.id.txtMailUser);
+//        mail.setText(user.getMail());
+        username = findViewById(R.id.txtUsernameUser);
+//        username.setText(user.getUsername());
 
-        if(auxMail.equals("")){
-            Intent it = new Intent(Main.this, Login.class);
-            startActivity(it);
-        } else {
-            User user = new User();
-            user.setMail(auxMail);
-            UserDAO uDAO = new UserDAO(getApplicationContext(), user);
-            user = uDAO.getUserByMail();
-
-            mail = findViewById(R.id.txtMailUser);
-            mail.setText(user.getMail());
-            username = findViewById(R.id.txtUsernameUser);
-            username.setText(user.getUsername());
-
-            btnAlter = findViewById(R.id.btnAlter);
-            btnDelete = findViewById(R.id.btnDelete);
-            btnLogout = findViewById(R.id.btnLoggout);
-
-            btnAlter.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent it = new Intent(Main.this, Update.class);
-                    startActivity(it);
-                }
-            });
-            btnDelete.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent it = new Intent(Main.this, Delete.class);
-                    startActivity(it);
-                }
-            });
-            btnLogout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    SharedPreferences.Editor editor = sp.edit();
-                    editor.clear();
-                    editor.commit();
-
-                    Intent it = new Intent(Main.this, Login.class);
-                    startActivity(it);
-                }
-            });
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null){
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            username.setText(personName);
+            mail.setText(personEmail);
         }
+
+
+
+        btnAlter = findViewById(R.id.btnAlter);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnLogout = findViewById(R.id.btnLoggout);
+
+        btnAlter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Main.this, Update.class);
+                startActivity(it);
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Main.this, Delete.class);
+                startActivity(it);
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sp.edit();
+                editor.clear();
+                editor.commit();
+
+                Intent it = new Intent(Main.this, Login.class);
+                startActivity(it);
+            }
+        });
+    }
+
+    void signOut(){
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(Main.this, Login.class));
+            }
+        });
     }
 }
